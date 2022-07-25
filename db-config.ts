2ce919c44778ce5cf7ef6458438000016161f5b4
Nawel Benaicha <nawelbenaicha@MBP-de-Nawel.lan>
2022-07-25 15:48:36 +0200

    logs heroku

--- db-config.ts
+++ db-config.ts
@@ -1,16 +1,24 @@
-// importe le package dotenv pour accèder au .env
-import 'dotenv/config';
-// importe mysql pour se connecter à la base
-import mysql, { Connection } from 'mysql2';
+import mysql, { Pool } from 'mysql2';
 
-// créer l'objet connection
-const connection: Connection = mysql.createConnection({
-  host: process.env.DB_HOST, // address of the server
-  port: Number(process.env.DB_PORT), // port of the DB server (mysql), not to be confused with the nodeJS server PORT !
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+// importe la variable d'environnement
+let databaseUrl: string = process.env.CLEARDB_DATABASE_URL || '';
+// retire le type de base de données
+databaseUrl = databaseUrl.substring(8);
+// who doesn't love some good old effective Regex ?
+const [user, password, host, database] = databaseUrl.split(
+  /[:@/?)<>{}[\]\r\n/\\]+/
+);
+
+// créer l'objet pool
+const pool: Pool = mysql.createPool({
+  host: host,
+  user: user,
+  password: password,
+  database: database,
+  port: 3306,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
-// exporte l'objet connection pour l'utiliser ailleurs
-export default connection;
+export default pool;
